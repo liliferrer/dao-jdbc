a0e7210a59771a5f49ae5942a26cd3c1e9187f9f
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-04-26 09:04:08 -0300

    Classe de entidades de departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,50 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import db.DB;
-import db.DBException;
-
-import java.sql.SQLException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String args[]) {
 		
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = DB.getConnection();
-			conn.setAutoCommit(false);
-			
-			st = conn.createStatement();
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-			
-			/*int x = 1;
-			if(x < 2) {
-				throw new SQLException("Fake error");
-			}*/
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-			
-			conn.commit();
-			
-			System.out.println("rows1: "+rows1);
-			System.out.println("rows2: "+rows2);
-		
-		}catch(SQLException e) {
-			
-			try {
-				conn.rollback();
-				throw new DBException("Transaction rolled back! Caused by: "+e.getMessage());
-				
-			}catch(SQLException e1) {
-				throw new DBException("Error trying to rollback! Caused by: "+e1.getMessage());
-			}
-		
-		}finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 	}
 }
