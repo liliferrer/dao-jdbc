fa5bcd2812f7084aacab5d7062da068d71d5776c
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-05-05 16:27:29 -0300

    Implementação do update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -64,8 +64,28 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"update seller "
+				   +"set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+				   +"where Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
 		
+		}catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
